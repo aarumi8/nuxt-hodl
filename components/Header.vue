fa6891ddd815806b6f26696411035a845d1db042
Max Rand <dopler168@gmail.com>
2024-02-06 23:29:00 +0300

    header, add 2 menus + css

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="header">
     <nuxt-link to="/" class="logo">HODL</nuxt-link>
+    <nuxt-link to="/" :class="['header-text', { 'active': isRouteActive('/') }]">Explore</nuxt-link>
+    <nuxt-link to="/portfolio" :class="['header-text', { 'active': isRouteActive('/portfolio') }]">Portfolio</nuxt-link>
     <div class="search-bar">
       <span class="search-icon"></span>
       <input type="text" placeholder="Search by token" />
@@ -11,7 +13,15 @@
 
 <script lang="ts">
 export default defineComponent({
-  // Component logic goes here
+  setup() {
+    const route = useRoute();
+
+    function isRouteActive(path: string): boolean {
+      return route.path === path;
+    }
+
+    return { isRouteActive };
+  }
 });
 </script>
 
@@ -32,10 +42,27 @@ export default defineComponent({
   text-decoration: none;
 }
 
+.header-text {
+  color: #FFF;
+  font-family: 'Gilroy', sans-serif;
+  font-weight: 600;
+  font-size: 1.25rem;
+  text-decoration: none;
+  opacity: 0.5;
+}
+
+.header-text.active {
+  opacity: 1.0;
+}
+
+.header-text:hover, .header-text:active {
+  opacity: 1.0;
+}
+
 .search-bar {
   display: flex;
   width: 100%;
-  padding: 1.85vh 2.08vw;
+  padding: 1.85vh 1.08vw;
   align-items: center;
   gap: 10px;
   border-radius: 15px;
@@ -77,5 +104,9 @@ input:focus {
   font-family: 'Gilroy';
   font-size: 1.25rem;
   font-weight: 500;
+}
+
+.connect-wallet:hover, .connect-wallet:active {
+  opacity: 0.75;
 }
 </style>
